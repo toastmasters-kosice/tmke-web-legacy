951a3f81591c5da2704252ee1e602fa7b8ba2cab
lukasbicus <ing.lukas.bicus@gmail.com>
2019-04-05 07:51:42 +0200

    Update menu texts

--- src/components/NavBar/NavGroup.js
+++ src/components/NavBar/NavGroup.js
@@ -4,13 +4,11 @@ import { routes } from '../../constants'
 
 const NavGroup = () =>
   <React.Fragment>
-    <NavLink to={routes.EDUCATION} p="12px 24px">Vzdelavanie</NavLink>
-    <NavLink to={routes.ABOUT} p="12px 24px">O nas</NavLink>
+    <NavLink to={routes.EDUCATION} p="12px 24px">Vzdelávanie</NavLink>
+    <NavLink to={routes.ABOUT} p="12px 24px">O nás</NavLink>
     <NavLink to={routes.MEETINGS} p="12px 24px">Stretnutia</NavLink>
     <NavLink to={routes.CONTACT} p="12px 24px">Kontakt</NavLink>
-    <NavLink to={routes.FOR_MEMBERS} p="12px 24px">Pre clenov</NavLink>
+    <NavLink to={routes.FOR_MEMBERS} p="12px 24px">Pre členov</NavLink>
   </React.Fragment>
-
-NavGroup.propTypes = {}
 
 export default NavGroup
