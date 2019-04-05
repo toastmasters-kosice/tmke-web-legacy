4f93d23aced1954776d77b679bfc9203a3c75f1a
lukasbicus <ing.lukas.bicus@gmail.com>
2019-04-05 11:57:46 +0200

    Update ForMembers filename to update route

--- src/pages/ForMembers.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ForMembers = () => (
-  <div>for members</div>
-)
-
-export default ForMembers
---
+++ src/pages/pre-clenov.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ForMembersPage = () => (
+  <div>for members</div>
+)
+
+export default ForMembersPage
